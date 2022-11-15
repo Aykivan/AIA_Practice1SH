b20e93d3beab3112b0d736b0d7c7d16ca8fda7af
Айкин Иван <ivan.aykin@mail.ru>
2022-11-15 23:36:55 +0700

    added task 07, 08

---
+++ 07/Program.cs
@@ -0,0 +1,13 @@
+﻿// Выяснить является ли число чётным
+
+System.Console.Write("Введите число а: ");
+int a=Convert.ToInt32(Console.ReadLine());
+if(a%2>0)
+{
+    System.Console.WriteLine("Число "+a+" является нечетным");
+}
+else
+{
+    System.Console.WriteLine("Число "+a+ " является четным");
+}
+//Осознанно делаю в задаче больше, чем требуется, с целью лучшего освоения материала
---
+++ 08/Program.cs
@@ -0,0 +1,16 @@
+﻿// Вывести на экран числа от -N до N
+
+System.Console.Write("Введите значение N: ");
+int N=Convert.ToInt32(Console.ReadLine());
+int i=N*-1;
+System.Console.WriteLine("Вывод чисел от -N до N, через цикл while:");
+while (i<=N)
+{
+    System.Console.Write($"{i} ");
+    i++;
+}
+System.Console.WriteLine();
+System.Console.WriteLine("Вывод чисел от -N до N, через цикл for:");
+for (int j=N*-1;j<=N;j++) System.Console.Write($"{j} ");
+
+//В этой задече я нашел решение, однако насколько оно правильно я не знаю, возможно есть способы проще, если есть пожалуйста напишите.
