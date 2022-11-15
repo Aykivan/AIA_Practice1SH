4cfb254fc05eac776cd6b595dff8b11cefea4342
Айкин Иван <ivan.aykin@mail.ru>
2022-11-15 22:50:40 +0700

    added task solution 03, 04, 05

---
+++ 03/Program.cs
@@ -0,0 +1,16 @@
+﻿// С клавиатуры вводя два числа a и b. Найти максимальное из них.
+
+System.Console.Write("Введите число а: ");
+int a=Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите число b: ");
+int b=Convert.ToInt32(Console.ReadLine());
+if(a>b)
+{
+    System.Console.WriteLine($"Наибольшее число: {a}");
+}
+else
+{
+    System.Console.WriteLine($"Наибольшее число: {b}");
+}
+
+//Нет уточнения в задачи по поводу равенства чисел. Поэтому, что делать программе в случае равенства я не прописывал.
---
+++ 05/Program.cs
@@ -0,0 +1,15 @@
+﻿// С клавиатуры вводятся три числа. Найти максимальное из трех чисел
+
+System.Console.Write("Введите первое число (а): ");
+int a=Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите второе число (b): ");
+int b=Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите третье число (c): ");
+int c=Convert.ToInt32(Console.ReadLine());
+int max=a;
+if (b>max) max=b;
+if (c>max) max=c;
+System.Console.WriteLine("При нахождении через условие if, наибольшее число равно: "+max);
+
+max = Math.Max(a, Math.Max(b, c)); //Нешел данный способ в гугле, когда моя программа не работа, решит тоже его закрепить
+System.Console.WriteLine("При нахождении, через класс Math, наибольшее число равно: "+max);
