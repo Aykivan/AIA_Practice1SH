c82cf1f544fb9e02c87f733bd3bedb0ad2d7f436
Айкин Иван <ivan.aykin@mail.ru>
2022-11-18 18:52:19 +0700

    added solution to basic algorithms

---
+++ BasikAlgorithms/01-Swap/Program.cs
@@ -0,0 +1,26 @@
+﻿// Обмен значениями 2х переменных
+
+/*Классический способ с 3тьей переменной
+int a = 5;
+int b = 7;
+int temp;
+
+temp=a;
+a=b;
+b=temp;*/
+
+//Без использования 3тьей переменной:
+/*int a=5;
+int b=7;
+a=a+b;
+b=a-b;
+a=a-b;
+System.Console.WriteLine(a+" "+b);*/
+
+//Загадочный способ, который не пояснили
+int a=5;
+int b=7;
+a=a^b;
+b=a^b;
+a=a^b;
+System.Console.WriteLine($"а={a} и b={b}");
